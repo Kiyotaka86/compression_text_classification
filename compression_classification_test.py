c80357cc55b829576e4c8305653ad87d5e3b684c
Kiyotaka Kokubun <22435430+Kiyotaka86@users.noreply.github.com>
2023-11-23 09:34:44 +0900

    Minor fixing for the function

--- compression_classification_test.py
+++ compression_classification_test.py
@@ -2,14 +2,14 @@ import numpy as np
 import gzip
 
 
-def compressed_classification(training_set, test_set):
+def compressed_classification(training_set, test_set, k):
     for (x1, _) in test_set:
         Cx1 = len(gzip.compress(x1.encode())) 
         distance_from_x1 = []
         for (x2, _) in training_set:
-            Cx2 = len(gzip.compress(x2.encode()) 
+            Cx2 = len(gzip.compress(x2.encode()))
             x1x2 = " ".join([x1, x2])
-            Cx1x2 = len(gzip.compress(x1x2.encode())
+            Cx1x2 = len(gzip.compress(x1x2.encode()))
             ncd = (Cx1x2 - min(Cx1,Cx2)) / max(Cx1, Cx2)
             distance_from_x1.append(ncd)
         sorted_idx = np.argsort(np.array(distance_from_x1))
