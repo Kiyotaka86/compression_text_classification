b740348bac02802a3a3be19212aef19d022e3898
Kiyotaka Kokubun <22435430+Kiyotaka86@users.noreply.github.com>
2023-12-04 17:47:16 +0900

    updated news data and knn

---
+++ part2/knn_test_d copy.py
@@ -0,0 +1,68 @@
+import numpy as np
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import json
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+
+def pass_data():
+
+    data = []
+
+    with open('News_Category_Dataset_v3.json') as file:
+        for line in file:
+            try:
+                data.append(json.loads(line))
+            except:
+                pass
+    
+    df = pd.DataFrame(data)
+    df['text'] = df['headline'] + ' ' + df['short_description']
+    df['label'] = df['category']
+
+    return df
+
+def get_data(test_size=0.2):
+    df = pass_data()
+
+    # Splitting the dataset
+    X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=test_size, random_state=42)
+
+    # Convert to numpy arrays
+    label_encoder = LabelEncoder()
+
+    train_text = X_train.to_numpy()
+    train_labels = label_encoder.fit_transform(y_train.to_numpy())
+    test_text = X_test.to_numpy()
+    test_labels = label_encoder.fit_transform(y_test.to_numpy())
+
+    train = (train_text, train_labels)
+    test = (test_text, test_labels)
+
+    return (train, test)
+
+# Assuming you have a function to load and split your data
+(train_text, train_labels), (test_text, test_labels) = get_data()
+
+# Text Vectorization
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(train_text)
+X_test = vectorizer.transform(test_text)
+
+# Convert labels to integers if they are categorical
+label_encoder = LabelEncoder()
+train_labels_encoded = label_encoder.fit_transform(train_labels)
+test_labels_encoded = label_encoder.transform(test_labels)
+
+# Train the standard KNN model
+standard_knn_model = KNeighborsClassifier()
+standard_knn_model.fit(X_train, train_labels_encoded)
+
+# Evaluate the standard KNN model
+standard_predictions = standard_knn_model.predict(X_test)
+print("Standard KNN Model Performance:")
+print(classification_report(test_labels_encoded, standard_predictions))
---
+++ part2/knn_test_d.py
@@ -0,0 +1,108 @@
+import numpy as np
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import json
+from npc_gzip.compressors.base import BaseCompressor
+from npc_gzip.compressors.gzip_compressor import GZipCompressor
+from npc_gzip.knn_classifier import KnnClassifier
+
+def pass_data():
+
+    data = []
+
+    with open('News_Category_Dataset_v3.json') as file:
+        for line in file:
+            try:
+                data.append(json.loads(line))
+            except:
+                pass
+    
+    df = pd.DataFrame(data)
+    df['text'] = df['headline'] + ' ' + df['short_description']
+    df['label'] = df['category']
+
+    return df
+
+def get_data(test_size=0.2):
+    df = pass_data()
+
+    # Splitting the dataset
+    X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=test_size, random_state=42)
+
+    # Convert to numpy arrays
+    label_encoder = LabelEncoder()
+
+    train_text = X_train.to_numpy()
+    train_labels = label_encoder.fit_transform(y_train.to_numpy())
+    test_text = X_test.to_numpy()
+    test_labels = label_encoder.fit_transform(y_test.to_numpy())
+
+    train = (train_text, train_labels)
+    test = (test_text, test_labels)
+
+    return (train, test)
+
+
+def fit_model(
+    train_text: np.ndarray, train_labels: np.ndarray, distance_metric: str = "ncd"
+) -> KnnClassifier:
+    """
+    Fits a Knn-GZip compressor on the train
+    data and returns it.
+
+    Arguments:
+        train_text (np.ndarray): Training dataset as a numpy array.
+        train_labels (np.ndarray): Training labels as a numpy array.
+
+    Returns:
+        KnnClassifier: Trained Knn-Compressor model ready to make predictions.
+    """
+
+    compressor: BaseCompressor = GZipCompressor()
+    model: KnnClassifier = KnnClassifier(
+        compressor=compressor,
+        training_inputs=train_text,
+        training_labels=train_labels,
+        distance_metric=distance_metric,
+    )
+
+    return model
+
+
+def main() -> None:
+    print("Fetching data...")
+    ((train_text, train_labels), (test_text, test_labels)) = get_data()
+
+    print("Fitting model...")
+    model = fit_model(train_text, train_labels)
+
+    # Randomly sampling from the test set.
+    # The IMDb test data comes in with all of the
+    # `1` labels first, then all of the `2` labels
+    # last, so we're shuffling so that our model
+    # has something to predict other than `1`.
+
+    random_indicies = np.random.choice(test_text.shape[0], 1000, replace=False)
+
+    sample_test_text = test_text[random_indicies]
+    sample_test_labels = test_labels[random_indicies]
+
+    print("Generating predictions...")
+    top_k = 1
+
+    # Here we use the `sampling_percentage` to save time
+    # at the expense of worse predictions. This
+    # `sampling_percentage` selects a random % of training
+    # data to compare `sample_test_text` against rather
+    # than comparing it against the entire training dataset.
+    (distances, labels, similar_samples) = model.predict(
+        sample_test_text, top_k, sampling_percentage=0.01
+    )
+
+    print(classification_report(sample_test_labels, labels.reshape(-1)))
+
+
+if __name__ == "__main__":
+    main()
